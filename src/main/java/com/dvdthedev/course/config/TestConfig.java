c5f580e0784556f8c8a32f8403d67edfc9181747
dvdthedev <dvd2.0@live.com>
2025-08-28 11:26:32 -0300

    add OrderStatus enum

--- src/main/java/com/dvdthedev/course/config/TestConfig.java
+++ src/main/java/com/dvdthedev/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.dvdthedev.course.config;
 
 import com.dvdthedev.course.entities.Order;
 import com.dvdthedev.course.entities.User;
+import com.dvdthedev.course.entities.enums.OrderStatus;
 import com.dvdthedev.course.repositories.OrderRepository;
 import com.dvdthedev.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.DELIVERED, u1);
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
