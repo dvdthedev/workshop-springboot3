5cc0bbf4c2ee5ee77cca843db3c8f59e7558ab5b
dvdthedev <dvd2.0@live.com>
2025-08-29 14:10:17 -0300

    payment one to one association

--- src/main/java/com/dvdthedev/course/entities/Order.java
+++ src/main/java/com/dvdthedev/course/entities/Order.java
@@ -30,6 +30,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -90,7 +93,13 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
 
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 
     public OrderStatus getOrderStatus() {
         return OrderStatus.valueOf(orderStatus);
